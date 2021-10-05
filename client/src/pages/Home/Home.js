1c9d932622bd7b3d3aa4c83c1f3f43100e3ef3f7
pranesh12 <praneshchakama6@gmail.com>
2021-10-05 17:06:39 +0600

    woriking good

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,13 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import About from "../../components/About/About";
 import Banner from "../../components/Banner/Banner";
-import BestTeachers from "../../components/BestTeachers/BestTeachers";
 import Footer from "../../components/Footer/Footer";
 import Information from "../../components/Information/Infromation";
 import Navbar from "../../components/Navbar/Navbar";
 import { getEducationalData, getTeachers } from "../../Redux/actions/education";
 
-import ImportTantCourse from "../Courses/ImportTantCourse";
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -21,8 +19,6 @@ const Home = () => {
       <Navbar />
       <Banner />
       <About />
-      <ImportTantCourse />
-      <BestTeachers />
       <Information />
       <Footer />
     </div>
